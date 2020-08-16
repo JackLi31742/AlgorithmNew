018b5e103cff1814d366e41985ea6e4091c71b81
Jackli31742 <317426838@qq.com>
2020-08-16 20:15:02 +0800

    更新了骑士周游问题

--- src/main/java/recursion/Knight.java
+++ src/main/java/recursion/Knight.java
@@ -5,7 +5,10 @@ public class Knight {
 		int n=6;
 		int[][]board=new int[n][n];
 		
-		KnightTour(board, 1, 2, n);
+		long start = System.currentTimeMillis();
+		KnightTour(board, 2, 1, n,2,1);
+		long end = System.currentTimeMillis();
+		System.out.println(end-start);
 		
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
@@ -15,30 +18,47 @@ public class Knight {
 		}
 	}
 
-	public static void KnightTour(int[][]board,int x,int y,int n) {
+	public static void KnightTour(int[][]board,int x,int y,int n,int startX,int startY) {
 		int[] xarr= {1,2,2,1,-1,-2,-2,-1};
 		int[] yarr= {-2,-1,1,2,2,1,-1,-2};
 		int step=0;
 		boolean[][]visited=new boolean[n][n];
 		board[x][y]=0;
 		visited[x][y]=true;
-		KnightTour(board, x, y, visited, step, xarr, yarr, n);
+		KnightTour(board, x, y, visited, step, xarr, yarr, n,startX,startY);
 		
 	}
 	
 	public static boolean KnightTour(int[][]board,int x,int y,
-			boolean[][]visited,int step,int[] xarr,int[] yarr,int n) {
+			boolean[][]visited,int step,int[] xarr,int[] yarr,int n,int startX,int startY) {
 		if (step==(n*n)-1) {
-			return true;
+			
+			for (int i = 0; i < xarr.length; i++) {
+				int nextX=x+xarr[i];
+				int nextY=y+yarr[i];
+				if (nextX==startX&&nextY==startY) {
+					
+					return true;
+				}
+			}
+			return false;
 		}
 		for (int i = 0; i < xarr.length; i++) {
 			int nextX=x+xarr[i];
 			int nextY=y+yarr[i];
 			if (check(nextX, nextY, visited, n)) {
+				//这里只是检查了是否合法，就将值改变，但其实不知道能不能走得完
 				board[nextX][nextY]=step+1;
 				visited[nextX][nextY]=true;
-				KnightTour(board, nextX, nextY, visited,step+1,xarr,yarr,n);
+				if (KnightTour(board, nextX, nextY, visited,step+1,xarr,yarr,n,startX,startY)) {
+					return true;
+				}else {
+					board[nextX][nextY]=0;//走不通，回溯
+					visited[nextX][nextY]=false;
+				}
+				
 			}
+
 		}
 		
 		return false;
@@ -51,4 +71,10 @@ public class Knight {
 			return false;
 		}
 	}
+	
+	
+	
+	
 }
+
+
