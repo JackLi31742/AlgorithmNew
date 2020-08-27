b00f868832a983f7883f4d6846b6f18fb30007a3
jackli31742 <317426838@qq.com>
2020-08-27 22:12:47 +0800

    更新了最长公共子串、最短路径等dp题目

--- src/main/java/dp/ShortestPath.java
+++ src/main/java/dp/ShortestPath.java
@@ -59,9 +59,9 @@ public class ShortestPath {
 			/*F*/ { N,   N,   N,   4,   5,   N,   6},
 			/*G*/ { 2,   3,   N,   N,   4,   6,   N}}; 
 
-		        System.out.println(minPath(m));;
-		        
-		        System.out.println(count);
+//		        System.out.println(minPath(m));;
+//		        
+//		        System.out.println(count);
 		        
 		        
 		        
@@ -72,7 +72,17 @@ public class ShortestPath {
 		                     };
 		        
 		        
-		        System.out.println(minPathSum(grid));;
+//		        System.out.println(minPathSum(grid));;
+		        
+		        
+		        int[][]grid1= {
+	                       {1,0}
+	                       
+	                     };
+//		        System.out.println(uniquePaths2(7, 3));
+//		        System.out.println(count);
+		        
+		        System.out.println(uniquePathsWithObstacles(grid1));
 	}
 	/***
 	 * 图的最短路径
@@ -194,5 +204,134 @@ public class ShortestPath {
 		}
 		
 		return distance[endX][endY];
+    }
+	
+	/**
+	 * 62. 不同路径
+	 * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+
+		机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+		
+		问总共有多少条不同的路径？
+
+		经过递推，f(m,n)=f(m-1,n)+f(m,n-1)
+	 * @param m
+	 * @param n
+	 * @return
+	 */
+	
+	public static int uniquePaths(int m, int n) {
+		if (m<=0||n<=0) {
+			return 0;
+		}
+		if (m==1||n==1) {
+			count++;
+			return 1;
+		}
+		
+		return uniquePaths(m-1, n)+uniquePaths(m, n-1);
+    }
+	
+	/**
+	 * 使用dp，dp[i][j]代表从起点走到该点的走法
+	 * @param m
+	 * @param n
+	 * @return
+	 */
+	public static int uniquePaths2(int m, int n) {
+		
+		if (m<=0||n<=0) {
+			return 0;
+		}
+		if (m==1||n==1) {
+			count++;
+			return 1;
+		}
+		
+		int[][]dp=new int[m][n];
+		
+		dp[0][0]=1;
+		
+		for (int i = 0; i < m; i++) {
+			dp[i][0]=1;
+		}
+		
+		for (int j = 0; j < n; j++) {
+			dp[0][j]=1;
+		}
+		
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j]=dp[i-1][j]+dp[i][j-1];
+			}
+		}
+		
+		return dp[m-1][n-1];
+	}
+	
+	/**
+	 * 63. 不同路径 II
+	 * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+	 * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+	 * @param obstacleGrid
+	 * @return
+	 */
+	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		if (obstacleGrid==null||obstacleGrid.length<=0||obstacleGrid[0].length<=0) {
+			return 0;
+		}
+		
+		int m=obstacleGrid.length;
+		int n=obstacleGrid[0].length;
+		
+		int[][]dp=new int[m][n];
+		
+		if (obstacleGrid[0][0]!=1) {
+			dp[0][0]=1;
+		}
+		
+		
+		for (int i = 0; i < m; i++) {
+			if (obstacleGrid[i][0]!=1) {
+				
+				dp[i][0]=1;
+			}else {
+				break;
+			}
+		}
+		
+		for (int j = 0; j < n; j++) {
+			
+			if (obstacleGrid[0][j]!=1) {
+				
+				dp[0][j]=1;
+			}else {
+				break;
+			}
+		}
+		
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				if (obstacleGrid[i][j]!=1) {
+					
+					dp[i][j]=dp[i-1][j]+dp[i][j-1];
+				}
+			}
+		}
+		
+		return dp[m-1][n-1];
+		
+    }
+	
+	/**
+	 * 62最后形成的树，很像杨辉三角
+	 * 118. 杨辉三角
+	 * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+	 * @param numRows
+	 * @return
+	 */
+	public List<List<Integer>> generate(int numRows) {
+		return null;
+
     }
 }
