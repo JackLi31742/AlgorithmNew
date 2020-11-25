20a2741f2e5b54c0f6cb40471e234c8f6d18e18c
jackli31742 <317426838@qq.com>
2020-11-25 22:23:57 +0800

    更新了分治法的查找子树和LCA

---
+++ src/main/java/second/divideconquer/ChangeTree.java
@@ -0,0 +1,26 @@
+package second.divideconquer;
+
+import tree.TreeNode;
+
+public class ChangeTree {
+	
+	/**
+	 * 175. 翻转二叉树
+	翻转一棵二叉树。左右子树交换
+	 * @param root
+	 */
+	public void invertBinaryTree(TreeNode root) {
+        // write your code here
+    }
+
+	
+	/**
+	 * 453. 将二叉树拆成链表
+	 * 将一棵二叉树按照前序遍历拆解成为一个 假链表。所谓的假链表是说，用二叉树的 right 指针，来表示链表中的 next 指针。
+	 * 不要忘记将左儿子标记为 null，否则你可能会得到空间溢出或是时间溢出。
+	 * @param root
+	 */
+	public void flatten(TreeNode root) {
+        // write your code here
+    }
+}
