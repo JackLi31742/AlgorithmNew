c65570cbd82f8c763ad631e2611da100a98c7616
jackli31742 <317426838@qq.com>
2020-12-16 21:19:13 +0800

    更新了二分矩阵 k 小

--- src/main/java/binarysearch/second/BinarySearch.java
+++ src/main/java/binarysearch/second/BinarySearch.java
@@ -9,7 +9,14 @@ public class BinarySearch {
 		int[]A= {123,123,123};
 		BinarySearch binarySearch=new BinarySearch();
 		
-		binarySearch.woodCut(A, 3);
+//		binarySearch.woodCut(A, 3);
+		
+		int[][]matrix= {{1,  5,  9},
+				{10, 11, 13},
+				{12, 13, 15}
+				};
+		
+		binarySearch.kthSmallest(matrix, 8);
 				
 	}
 	
@@ -700,13 +707,27 @@ public class BinarySearch {
 	/**
 	 * 面试题 10.09. 排序矩阵查找
 	 * 给定M×N矩阵，每一行、每一列都按升序排列，请编写代码找出某元素。
+	 * 
+	 * [
+		  [1,   4,  7, 11, 15],
+		  [2,   5,  8, 16, 19],
+		  [3,   6,  9, 17, 22],
+		  [10, 13, 14, 18, 24],
+		  [18, 21, 23, 26, 30]
+		]
+
+	 * 从右上角15，如果15<target，往下走一行，（即使19的左边有比15大的，比如16，那也一定比19小，所以会走到左边）
+	 * 如果15>target，往左走一行，说明target在左边
+	 * 
+	 * 从左下角，18<target，往右走
+	 * 18>target，往上走
 	 * @param matrix
 	 * @param target
 	 * @return
 	 */
 	public boolean searchMatrix(int[][] matrix, int target) {
 
-		if (matrix==null||matrix.length<=0||matrix[0].length<=0||k<=0) {
+		if (matrix==null||matrix.length<=0||matrix[0].length<=0) {
 			return false;
 		}
 		
@@ -714,27 +735,37 @@ public class BinarySearch {
 		
 		int col = matrix[0].length;
 		
-		int left=0;
-		int right=row*col-1;
+		int x=0;int y=col-1;
 		
-		while(left<=right) {
-			
-			int mid=left+(right-left)/2;
-			
-			if (matrix[mid/col][mid%col]<target) {
-				
+		while(x<row&&y>=0) {
+			if (matrix[x][y]<target) {
+				x++;
+			}else if (matrix[x][y]>target) {
+				y--;
+			}else {
+				return true;
 			}
 		}
+		
+		return false;
 		
     }
 	
 	/**
+	 * 
+	 * 378
 	 * 401. 排序矩阵中的从小到大第k个数
 		在一个排序矩阵中找从小到大的第 k 个整数。
 
 排序矩阵的定义为：每一行递增，每一列也递增。
 
-	没有利用到矩阵本身递增的性质
+	要想利用矩阵本身的性质，这是在答案上的二分
+	
+	为什么是答案呢，意思就是说矩阵的左上角和右下角是范围
+	所有的数都在这里，取中点之后，需要找到比这个点小的有多少个，大的也就知道了
+	而求有多少个比这个点小，就用searchMatrix的方法
+	
+	
 	 * @param matrix
 	 * @param k
 	 * @return
@@ -749,19 +780,78 @@ public class BinarySearch {
 		
 		int col = matrix[0].length;
 		
-		int left=0;
-		int right=row*col-1;
+		int left=matrix[0][0];
+		int right=matrix[row-1][col-1];
 		
-		while(left<=right) {
+		while(left <= right) {
 			
 			int mid=left+(right-left)/2;
 			
-			if (matrix[mid/col][mid%col]) {
+			Result result=getNumSmallerThanMid(matrix, mid, row, col);
+			
+			//不能把判断直接放到这，否则会死循环
+//			if (result.exist) {
 				
-			}
+				if (result.num<k) {
+					left=mid+1;
+				}else if (result.num>k) {
+					right=mid-1;
+				}else {
+					if (result.exist) {
+						return mid;
+					}else {
+						//由于是找小的，所以要往左走
+						right=mid-1;
+					}
+				}
 		}
+		
+		
+		return left;
 	}
 	
+	/**
+	 * target就是每次的mid，
+	 * 返回值要把target在不在矩阵中，以及对应比它小的数量带回去
+	 * @param matrix
+	 * @return
+	 */
+	public Result getNumSmallerThanMid(int[][] matrix,int target,int row,int col) {
+		
+		int x=0;int y=col-1;
+		
+		int num=0;
+		boolean exist=false;
+		while(x<row&&y>=0) {
+			if (matrix[x][y]==target) {
+				exist=true;
+			}
+			if (matrix[x][y]<=target) {
+				
+				x++;
+				//要把所有小于它的都加上，和找等于target的不一样
+				num+=y+1;
+			}else {
+				y--;
+			}
+		}
+		
+		return new Result(exist, num);
+	}
 	
+}
+
+class Result{
+	boolean exist;
+	int num;
+	public Result(boolean exist, int num) {
+		super();
+		this.exist = exist;
+		this.num = num;
+	}
+	@Override
+	public String toString() {
+		return "Result [exist=" + exist + ", num=" + num + "]";
+	}
 	
 }
