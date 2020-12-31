5a1b12b87889fc0d8a488265c7ae207cc694db93
jackli31742 <317426838@qq.com>
2020-12-31 10:28:08 +0800

    更新了系统设计

---
+++ src/main/java/design/system/ConsistentHashing2.java
@@ -0,0 +1,31 @@
+package design.system;
+
+import java.util.List;
+
+/**
+ * 520. 一致性哈希 II
+
+ *
+ */
+public class ConsistentHashing2 {
+
+	public static ConsistentHashing2 create(int n, int k) {
+        // Write your code here
+    }
+
+    /*
+     * @param machine_id: An integer
+     * @return: a list of shard ids
+     */
+    public List<Integer> addMachine(int machine_id) {
+        // write your code here
+    }
+
+    /*
+     * @param hashcode: An integer
+     * @return: A machine id
+     */
+    public int getMachineIdByHashCode(int hashcode) {
+        // write your code here
+    }
+}
---
+++ src/main/java/design/system/WebLogger.java
@@ -0,0 +1,35 @@
+package design.system;
+
+/**
+ * 505. 网页日志
+ * 描述
+实现下面两个方法：
+1.hit(timestamp) 建立一个时间戳
+2.get_hit_count_in_last_5_minutes(timestamp) 得到最后5分钟时间戳个数
+
+timestamp的单位是秒
+
+对函数的调用是按照时间顺序的, 就是说, timestamp是递增的
+ *
+ */
+public class WebLogger {
+    public WebLogger() {
+        // do intialization if necessary
+    }
+
+    /*
+     * @param timestamp: An integer
+     * @return: nothing
+     */
+    public void hit(int timestamp) {
+        // write your code here
+    }
+
+    /*
+     * @param timestamp: An integer
+     * @return: An integer
+     */
+    public int get_hit_count_in_last_5_minutes(int timestamp) {
+        // write your code here
+    }
+}
